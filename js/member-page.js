4fe1d587de94d0285747c61c6d2ba86e095d7975
II-Zor-II <winzorJMpaelmo@yahoo.com.ph>
2017-05-14 04:06:07 +0800

    finished the ui for forms

--- js/member-page.js
+++ js/member-page.js
@@ -1,7 +1,26 @@
+//forms
+//$("#timer-form").hide();
+
+
+
 $(document).ready(function(){
 	
 	$("#mem-myWorklog").click(function(){
 		window.location.href="/tmq/member-personalWorkLog.php?user_id="+$(this).attr("value")+"&username="+$(this).attr("name");
 	});
 	
+	
+	$("#myTab a").click(function(e){
+    	e.preventDefault();
+    	$(this).tab('show');
+    });
+	
+	$("#selct-timr-btn").click(function(){
+		//$("time-form").hide.$("#timer-form").show();
+		$.when($("#time-form").hide()).then($("#timer-form").show());
+	});
+	$("#selct-time-btn").click(function(){
+		//$("time-form").hide.$("#timer-form").show();
+		$.when($("#timer-form").hide()).then($("#time-form").show());
+	});
 });
--- member-personalWorkLog.php
+++ member-personalWorkLog.php
@@ -21,15 +21,54 @@ include_once 'header.php';
 	</div>
 	<div class="row">
 			<label>Clock in</label>
-			<input disabled/ type="time"/>	
+			<input type="time" />	
 	</div>
+
 	<div class="row time-timer-container">
-		
-		
+			<h3><strong>Log</strong></h3>
+			<button id="selct-time-btn" class="btn btn-primary">Select by Time</button>
+			<button id="selct-timr-btn" class="btn btn-primary">Select by Timer</button>
+		<hr>
+		<table id="time-form" class="table table-bordered">
+		 	<tr>
+		 		<td><input type="time" /></td>		 		
+		 	</tr>
+		 
+			 <tr>
+                <td>Location</td>
+                <td><input type="text" class="form-control"/></td>
+            </tr> 
+            <tr>
+                <td>Collaborated with</td>
+                <td><input type="text" class="form-control"/></td>
+            </tr> 
+            <tr>
+                <td>Description</td>
+				<td><textarea class="form-control" rows="5"></textarea></td>
+            </tr> 
+			
+		</table>
+		<table id="timer-form" style="display:none">
+			<tr>
+				<td>
+					<input id="elapsed-timer" type="text" placeholder="00:00" class="text-center" disabled/>
+				</td>
+				<td>
+					<button id="start-pause-btn" class="btn btn-primary">Start</button>
+				</td>
+				<td>
+					<button id="reset-btn" class="btn btn-danger">Reset</button>
+				</td>
+				
+			</tr>
+			
+		</table>
+
 	</div>
+		<hr>
 		<div>
 			<button class="btn btn-danger" id="Cancel">Cancel</button>
-			<input type="submit" class="btn btn-primary" name="submit" value="Add" />
+			<input type="submit" class="btn btn-primary" name="submit" value="Save" />
 		</div>
 </div>
 <?php 
